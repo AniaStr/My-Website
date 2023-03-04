7d41cca99fa8d8a58a79018e83baab544f5c0bab
DESKTOP-SFE4M0U\Admin <straczek.anna@gmail.com>
2023-03-04 19:05:24 +0100

    js functionality add

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ function resetLinks() {
         li.classList.remove('active')
     })
 };
+
+
+
+const addActiveClass = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const currentLink = document.querySelector(`.nav-item a[href='#${entry.target.id}']`);
+            if (currentLink?.getAttribute("data-link") == entry.target.id)
+                currentLink.classList.add("active");
+            resetLinks()
+        }
+        else if (!entry.isIntersecting) {
+            const currentLink2 = document.querySelector(`.nav-item a[href='#${entry.target.id}']`)
+            if (currentLink2?.classList)
+                currentLink2.classList.remove("active")
+        }
+    });
+};
+
+const options = {
+    threshold: 0.6,
+};
+
+const observer = new IntersectionObserver(addActiveClass, options);
+
+const section = document.querySelectorAll('section');
+section.forEach((section) => {
+    observer.observe(section);
+})
